b321086e2e91254e6cb4ff813ab8545d4f972668
kmltuzer <kmltuzer@gmail.com>
2023-09-15 16:26:59 +0300

    a func into a hook

--- components/ShowCase/GenresStripe/GenresStripe.tsx
+++ components/ShowCase/GenresStripe/GenresStripe.tsx
@@ -3,8 +3,7 @@
 import Genre from "./Genre";
 import {FaAngleLeft, FaAngleRight} from 'react-icons/fa'
 import './style.css';
-import { useEffect } from "react";
-import { callMeBabyAfterComponentIsMounted } from "@/utils/events";
+import { useSlider } from "@/utils/hooks";
 
 type Props = {
   genres :   {id: number, name: string}[],
@@ -12,11 +11,7 @@ type Props = {
 }
 
 function GenresStripe({genres, type}: Props) {
-
-  useEffect(() => {
-    callMeBabyAfterComponentIsMounted()
-  }, [])
-
+  const slider = useSlider({ parent: '.genres', arrows: '.shadow' });
   return (
     <div className={"genres-stripe-wrapper"}>
       <div className="shadow left">
