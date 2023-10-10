87de3a544e6c8b7dc3dd409f6e9f7848d1241e0a
ademmeral <kmltuzer@gmail.com>
2023-10-10 23:41:23 +0100

    small changes

--- api/genres.ts
+++ api/genres.ts
@@ -1,10 +1,7 @@
 import Paxios from './api';
 
-export async function getCategories(param:string): Promise<Categories | boolean>{
-    try{
-      const resp = await Paxios.get(`genre/${param}/list?language=en`)
+export async function getCategories(param:string): Promise<Categories | undefined>{
+      const resp = await Paxios.get(`genre/${param}/list?language=en`);
+      if (!resp.ok) throw new Error('Failed to fetch data');
       return await resp.json()
-    } catch (err) {
-      return false;
-    }
   }
--- api/items.ts
+++ api/items.ts
@@ -1,26 +1,19 @@
 import Paxios from './api';
 
-export async function getMany(param:string):Promise<Movies | Series | boolean>{
-    try{
-      const resp = await Paxios.get(`${param}?language=en-US&page=1`)
-      return await resp.json()
-    } catch (err) {
-      return false
-    }
-  }
-  export async function getOne(param:string): Promise<SingleItemType | boolean>{
-    try{
-      const resp = await Paxios.get(`${param}?language=en-US`)
-      return await resp.json()
-    } catch (err) {
-      return false
-    }
-  }
-  export async function getByCategory(type:string, id:string): Promise<Movies | Series | boolean>{
-    try{
-      const resp = await Paxios.get(`discover/${type}?language=en-US&page=1&with_genres=${id}`)
-      return await resp.json()
-    } catch (err) {
-      return false;
-    }
-  }
+export async function getMany(param: string): Promise<Movies | Series | undefined> {
+  const resp = await Paxios.get(`${param}?language=en-US&page=1`);
+  if (!resp.ok) throw new Error('Failed to fetch data');
+  return await resp.json()
+}
+export async function getOne(param: string): Promise<SingleItemType | undefined> {
+
+  const resp = await Paxios.get(`${param}?language=en-US`);
+  if (!resp.ok) throw new Error('Failed to fetch data');
+  return await resp.json()
+}
+export async function getByCategory(type: string, id: string): Promise<Movies | Series | undefined> {
+
+  const resp = await Paxios.get(`discover/${type}?language=en-US&page=1&with_genres=${id}`)
+  if (!resp.ok) throw new Error('Failed to fetch data');
+  return await resp.json()
+}
